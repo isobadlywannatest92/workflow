8c1d57ff631af75659f77d4ed44b46afe84c4fc5
isobadlywannatest92 <142501062+isobadlywannatest92@users.noreply.github.com>
2025-04-22 12:12:18 +0200

    Update main.tf

--- infra/modules/test-module/main.tf
+++ infra/modules/test-module/main.tf
@@ -1,4 +1,8 @@
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "example" {
-  bucket = var.bucket_name
+  bucket = "${var.bucket_name}-${random_id.suffix.hex}"
   tags   = var.bucket_tags
 }
