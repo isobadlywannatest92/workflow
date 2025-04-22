4ae2c88f9a6c264ee423c46750f852644cf004e2
isobadlywannatest92 <142501062+isobadlywannatest92@users.noreply.github.com>
2025-04-22 11:48:19 +0200

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,11 +1,14 @@
+# main.tf
+
 provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = var.bucket_name
-
-  tags = {
-    "Environment" = "Test"
+module "example_s3_bucket" {
+  source      = "./modules/s3_bucket"
+  bucket_name = var.bucket_name
+  bucket_tags = {
+    Environment = "Test"
+    Team        = "DevOps"
   }
 }
