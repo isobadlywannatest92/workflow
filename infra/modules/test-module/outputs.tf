d6bcfa4c91dbae08a19d789023602eb5704bf7df
isobadlywannatest92 <142501062+isobadlywannatest92@users.noreply.github.com>
2025-04-22 12:14:48 +0200

    Update outputs.tf

--- infra/modules/test-module/outputs.tf
+++ infra/modules/test-module/outputs.tf
@@ -1,9 +1,7 @@
 output "bucket_name" {
-  description = "The name of the S3 bucket"
-  value       = aws_s3_bucket.example.bucket
+  value = aws_s3_bucket.example.bucket
 }
 
 output "bucket_arn" {
-  description = "The ARN of the S3 bucket"
-  value       = aws_s3_bucket.example.arn
+  value = aws_s3_bucket.example.arn
 }
