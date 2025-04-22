61811f1b475bdfe535df44e000962f69713f4020
isobadlywannatest92 <142501062+isobadlywannatest92@users.noreply.github.com>
2025-04-22 12:15:06 +0200

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,11 +1,7 @@
 variable "aws_region" {
-  description = "AWS region to deploy to"
-  type        = string
-  default     = "us-east-1"
+  default = "us-east-1"
 }
 
 variable "bucket_name" {
-  description = "The name of the S3 bucket"
-  type        = string
-  default     = "my-env0-example-bucket"
+  default = "my-env0-example-bucket"
 }
